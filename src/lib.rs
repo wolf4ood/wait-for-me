947f6be7095ebf2a012f6150d63eb07fa5ae02f9
wolf4ood <maggiolo00@gmail.com>
2020-04-24 22:43:57 +0200

    Added usage of mutex

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 #![forbid(unsafe_code)]
 use std::future::Future;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 
 use std::pin::Pin;
 use std::task::{Context, Poll, Waker};
 
 pub fn latch(count: usize) -> (Waiter, Latch) {
-    let count = Arc::new(AtomicUsize::new(count));
+    let count = Arc::new(Mutex::new(count));
     let latch = Latch {
         count: count.clone(),
         waker: Arc::new(Mutex::new(None)),
@@ -22,7 +21,7 @@ pub fn latch(count: usize) -> (Waiter, Latch) {
 }
 
 pub struct Waiter {
-    count: Arc<AtomicUsize>,
+    count: Arc<Mutex<usize>>,
     latch: Latch,
 }
 
@@ -37,8 +36,8 @@ struct WaiterFuture(Waiter);
 impl Future for WaiterFuture {
     type Output = Result<(), ()>;
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let val = self.0.count.load(Ordering::Relaxed);
-        if val == 0 {
+        let val = self.0.count.lock().unwrap();
+        if *val == 0 {
             Poll::Ready(Ok(()))
         } else {
             let mut guard = self.0.latch.waker.lock().unwrap();
@@ -50,16 +49,27 @@ impl Future for WaiterFuture {
 
 #[derive(Clone)]
 pub struct Latch {
-    count: Arc<AtomicUsize>,
+    count: Arc<Mutex<usize>>,
     waker: Arc<Mutex<Option<Waker>>>,
 }
 
 impl Latch {
     pub fn count_down(self) {
-        let val = self.count.fetch_sub(1, Ordering::Relaxed);
-        if val == 0 {
-            let mut guard = self.waker.lock().unwrap();
-            guard.take().unwrap().wake();
+        let mut val = self.count.lock().unwrap();
+        match *val {
+            1 => {
+                *val -= 1;
+                drop(val);
+                let mut guard = self.waker.lock().unwrap();
+                match guard.take() {
+                    Some(waker) => waker.wake(),
+                    None => {}
+                }
+            }
+            n @ _ if n > 0 => {
+                *val -= 1;
+            }
+            _ => {}
         }
     }
 }
